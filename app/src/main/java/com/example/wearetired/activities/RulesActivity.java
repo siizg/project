a17857a6ad3b20467235fa2fbedf87b6a8c90f61
sa_su <sa_surkova@mail.ru>
2022-06-04 12:53:17 +0300

    ff

--- app/src/main/java/com/example/wearetired/activities/RulesActivity.java
+++ app/src/main/java/com/example/wearetired/activities/RulesActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 import com.example.wearetired.R;
 
 public class RulesActivity extends AppCompatActivity {
-    String rules = "Rules:\n" +
+    String rules =
             "  The rules are very simple!  You need to put crosses or zeroes on the board 3x3 in such a way that the horizon gets three in a row (vertically, horizontally or diagonally).  By getting one of the those combinations from your signs, you win.\n" +
             "  In the Single player mode you will play with a computer. For a victory you will get 20 cups, for a draw 10 cups, for the loss 0 cups.\n" +
             "  In the \"Two players\" mode you need to play with a person 1 on 1, in which everyone goes beyond their sign (cross or zero).  Cups are not awarded in this mode.\n" +
